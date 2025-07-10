92bb86dbb5386b1f579bc6dd66172352209c769c
yarbschris <123770805+yarbschris@users.noreply.github.com>
2025-07-10 16:03:12 -0500

    Some fixes and additions
    Fixed keymaps, added bufferline, lualine, and telescope

--- lua/chris/plugins/which-key.lua
+++ lua/chris/plugins/which-key.lua
@@ -8,13 +8,4 @@ return {
     opts = {
 
     },
-    keys = {
-        {
-            "<leader>?",
-            function()
-                require("which-key").show({ global = false })
-            end,
-            desc = "Buffer Local Keymaps (which-key)",
-        },
-    },
 }
