bbbe031bc3db79bc64c0d5ed31034a4e0810bb75
yarbschris <123770805+yarbschris@users.noreply.github.com>
2025-07-10 16:44:50 -0500

    Added Pluugins
    Dressing, indent, autocomplete, treesitter, vim-maximizer

--- lua/chris/lazy.lua
+++ lua/chris/lazy.lua
@@ -11,4 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("chris.plugins")
+require("lazy").setup("chris.plugins", {
+    checker = {
+        enabled = true,
+        notify = false,
+    },
+    change_detection = {
+        notify = false,
+    },
+})
