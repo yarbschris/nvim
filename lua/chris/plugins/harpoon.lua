7599e6fc737a9e90d47168c27a25dd4b5843a688
yarbschris <123770805+yarbschris@users.noreply.github.com>
2025-07-12 19:35:10 -0500

    Added Harpoon

---
+++ lua/chris/plugins/harpoon.lua
@@ -0,0 +1,45 @@
+return {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { { "nvim-lua/plenary.nvim" } },
+
+    config = function()
+        local harpoon = require("harpoon")
+
+        harpoon:setup()
+        local keymap = vim.keymap
+
+        keymap.set("n", "<leader>a", function()
+            harpoon:list():add()
+        end, { desc = "Add file to harpoon list" })
+
+        keymap.set("n", "<leader>bx", function()
+            harpoon:list():remove()
+        end, { desc = "Remove file in current buffer from harpoon list " })
+
+        keymap.set("n", "<leader>1", function()
+            harpoon:list():select(1)
+        end, { desc = "Select Harpoon 1" })
+
+        keymap.set("n", "<leader>2", function()
+            harpoon:list():select(2)
+        end, { desc = "Select Harpoon 2" })
+
+        keymap.set("n", "<leader>3", function()
+            harpoon:list():select(3)
+        end, { desc = "Select Harpoon 3" })
+
+        keymap.set("n", "<leader>4", function()
+            harpoon:list():select(4)
+        end, { desc = "Select Harpoon 4" })
+
+        -- Toggle previous & next buffers stored within Harpoon list
+        keymap.set("n", "<leader>bp", function()
+            harpoon:list():prev()
+        end, { desc = "Toggle next buffer in Harpoon list" })
+
+        keymap.set("n", "<leader>bn", function()
+            harpoon:list():next()
+        end, { desc = "Toggle previous buffer in Harpoon list" })
+    end,
+}
--- lua/chris/plugins/telescope.lua
+++ lua/chris/plugins/telescope.lua
@@ -1,37 +1,64 @@
 return {
-  "nvim-telescope/telescope.nvim",
-  branch = "0.1.x",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-    "nvim-tree/nvim-web-devicons",
-    "folke/todo-comments.nvim",
-  },
-  config = function()
-    local telescope = require("telescope")
-    local actions = require("telescope.actions")
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        "nvim-tree/nvim-web-devicons",
+        "folke/todo-comments.nvim",
+    },
+    config = function()
+        local telescope = require("telescope")
+        local actions = require("telescope.actions")
 
-    telescope.setup({
-      defaults = {
-        path_display = { "smart" },
-        mappings = {
-          i = {
-            ["<C-k>"] = actions.move_selection_previous, -- move to prev result
-            ["<C-j>"] = actions.move_selection_next, -- move to next result
-            ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
-          },
-        },
-      },
-    })
+        telescope.setup({
+            defaults = {
+                path_display = { "smart" },
+                mappings = {
+                    i = {
+                        ["<C-k>"] = actions.move_selection_previous, -- move to prev result
+                        ["<C-j>"] = actions.move_selection_next, -- move to next result
+                        ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                    },
+                },
+            },
+        })
 
-    telescope.load_extension("fzf")
+        telescope.load_extension("fzf")
 
-    -- set keymaps
-    local keymap = vim.keymap -- for conciseness
+        -- set keymaps
+        local keymap = vim.keymap -- for conciseness
 
-    keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
-    keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
-  end,
+        keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+        keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+        keymap.set("n", "<leader>fs", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+        keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+
+        local harpoon = require("harpoon")
+        harpoon:setup({})
+
+        -- basic telescope configuration
+        local conf = require("telescope.config").values
+        local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files.items) do
+                table.insert(file_paths, item.value)
+            end
+
+            require("telescope.pickers")
+                .new({}, {
+                    prompt_title = "Harpoon",
+                    finder = require("telescope.finders").new_table({
+                        results = file_paths,
+                    }),
+                    previewer = conf.file_previewer({}),
+                    sorter = conf.generic_sorter({}),
+                })
+                :find()
+        end
+
+        vim.keymap.set("n", "<leader>fh", function()
+            toggle_telescope(harpoon:list())
+        end, { desc = "Open harpoon window" })
+    end,
 }
