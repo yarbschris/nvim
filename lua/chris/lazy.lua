9640690330e9c359bc404e05290407453c8a4d97
yarbschris <123770805+yarbschris@users.noreply.github.com>
2025-08-05 00:32:07 -0500

    Update lazy.lua
    Added DAP folder to imports in lazy.lua

--- lua/chris/lazy.lua
+++ lua/chris/lazy.lua
@@ -11,12 +11,15 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "chris.plugins" }, { import = "chris.plugins.lsp"} }, {
-    checker = {
-        enabled = true,
-        notify = false,
-    },
-    change_detection = {
-        notify = false,
-    },
-})
+require("lazy").setup(
+    { { import = "chris.plugins" }, { import = "chris.plugins.lsp" }, { import = "chris.plugins.dap" } },
+    {
+        checker = {
+            enabled = true,
+            notify = false,
+        },
+        change_detection = {
+            notify = false,
+        },
+    }
+)
