94d8fb7f6b65b242957624535df21771a3c6480d
yarbschris <123770805+yarbschris@users.noreply.github.com>
2025-11-30 11:38:07 -0600

    Add <space><space> for telescope, fix keymaps and clipboard settings

--- lua/chris/core/options.lua
+++ lua/chris/core/options.lua
@@ -30,7 +30,7 @@ opt.signcolumn = "yes" -- show sign column so that text doesn't shift
 opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line, or start pos
 
 --clipboard
--- opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 
 --split windows
 opt.splitright = true -- split vertical window to right
