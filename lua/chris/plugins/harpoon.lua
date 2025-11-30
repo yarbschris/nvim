8454f8e903db84a6abe422d7e7fc942612809eb2
yarbschris <123770805+yarbschris@users.noreply.github.com>
2025-11-30 11:36:29 -0600

    Fix Harpoon and make it easier to use

--- lua/chris/plugins/harpoon.lua
+++ lua/chris/plugins/harpoon.lua
@@ -1,45 +1,42 @@
 return {
     "ThePrimeagen/harpoon",
     branch = "harpoon2",
-    dependencies = { { "nvim-lua/plenary.nvim" } },
+    opts = {
+        menu = {
+            width = vim.api.nvim_win_get_width(0) - 4,
+        },
+        settings = {
+            save_on_toggle = true,
+        },
+    },
+    keys = function()
+        local keys = {
+            {
+                "<leader>a",
+                function()
+                    require("harpoon"):list():add()
+                end,
+                desc = "Harpoon File",
+            },
+            {
+                "<leader>A",
+                function()
+                    local harpoon = require("harpoon")
+                    harpoon.ui:toggle_quick_menu(harpoon:list())
+                end,
+                desc = "Harpoon Quick Menu",
+            },
+        }
 
-    config = function()
-        local harpoon = require("harpoon")
-
-        harpoon:setup()
-        local keymap = vim.keymap
-
-        keymap.set("n", "<leader>a", function()
-            harpoon:list():add()
-        end, { desc = "Add file to harpoon list" })
-
-        keymap.set("n", "<leader>bx", function()
-            harpoon:list():remove()
-        end, { desc = "Remove file in current buffer from harpoon list " })
-
-        keymap.set("n", "<leader>1", function()
-            harpoon:list():select(1)
-        end, { desc = "Go to Harpoon File 1" })
-
-        keymap.set("n", "<leader>2", function()
-            harpoon:list():select(2)
-        end, { desc = "Go to Harpoon File 2" })
-
-        keymap.set("n", "<leader>3", function()
-            harpoon:list():select(3)
-        end, { desc = "Go to Harpoon File 3" })
-
-        keymap.set("n", "<leader>4", function()
-            harpoon:list():select(4)
-        end, { desc = "Go to Harpoon File 4" })
-
-        -- Toggle previous & next buffers stored within Harpoon File list
-        keymap.set("n", "<leader>bp", function()
-            harpoon:list():prev()
-        end, { desc = "Toggle next buffer in Harpoon File list" })
-
-        keymap.set("n", "<leader>bn", function()
-            harpoon:list():next()
-        end, { desc = "Toggle previous buffer in Harpoon File list" })
+        for i = 1, 9 do
+            table.insert(keys, {
+                "<leader>" .. i,
+                function()
+                    require("harpoon"):list():select(i)
+                end,
+                --desc = "Harpoon to File " .. i,
+            })
+        end
+        return keys
     end,
 }
