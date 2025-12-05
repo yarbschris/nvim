03de9f66091656fa9d2b0f062583efffb2e62dae
yarbschris <123770805+yarbschris@users.noreply.github.com>
2025-12-05 11:49:56 -0600

    Change explorer keymaps

--- lua/chris/plugins/nvim-tree.lua
+++ lua/chris/plugins/nvim-tree.lua
@@ -49,14 +49,14 @@ return {
         -- set keymaps
         local keymap = vim.keymap
 
-        keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-        keymap.set(
-            "n",
-            "<leader>ef",
-            "<cmd>NvimTreeFindFileToggle<CR>",
-            { desc = "Toggle file explorer on current dir" }
-        )
-        keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
-        keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+        keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+        -- keymap.set(
+        --     "n",
+        --     "<leader>ef",
+        --     "<cmd>NvimTreeFindFileToggle<CR>",
+        --     { desc = "Toggle file explorer on current dir" }
+        -- )
+        -- keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+        -- keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
     end,
 }
